c159b517b90d17bfd8a659d78629579d03f0c846
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-03-12 18:33:01 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/jeonseguard/backend/JeonseGuardBackendApplication.java
@@ -0,0 +1,12 @@
+package jeonseguard.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeonseGuardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeonseGuardBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/jeonseguard/backend/JeonseGuardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jeonseguard.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeonseGuardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
