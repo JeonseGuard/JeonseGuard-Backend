e1503275a0d90acfcd0b2d6e4e25641eddb60872
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-03-22 15:48:00 +0900

    chore: BoardController @Tag 수정(#9)

--- src/main/java/jeonseguard/backend/board/presentation/controller/BoardController.java
+++ src/main/java/jeonseguard/backend/board/presentation/controller/BoardController.java
@@ -12,7 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "Post", description = "게시판 관련 API")
+@Tag(name = "Board", description = "게시판 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/board")
