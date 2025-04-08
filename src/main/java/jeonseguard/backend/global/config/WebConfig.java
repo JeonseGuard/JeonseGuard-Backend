19c6ae9c726f35daa40a5418ea0d7531ee6341e1
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-04-08 23:04:31 +0900

    refactor: JwtTokenProvider에 JwtTokenProperties 적용하여 설정 분리(#28)
    - @Value 기반 설정을 ConfigurationProperties 기반으로 리팩토링
    - WebConfig에 @EnableConfigurationProperties 추가

--- src/main/java/jeonseguard/backend/global/config/WebConfig.java
+++ src/main/java/jeonseguard/backend/global/config/WebConfig.java
@@ -1,7 +1,9 @@
 package jeonseguard.backend.global.config;
 
 import jeonseguard.backend.auth.infrastructure.resolver.AuthenticatedUserArgumentResolver;
+import jeonseguard.backend.global.config.properties.JwtTokenProperties;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,6 +12,7 @@ import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableConfigurationProperties(JwtTokenProperties.class)
 public class WebConfig implements WebMvcConfigurer {
     private final AuthenticatedUserArgumentResolver authenticatedUserArgumentResolver;
 
