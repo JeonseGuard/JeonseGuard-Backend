0e4b3bd854a1d147715c5117a5dcf142b839315d
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-04-08 19:45:51 +0900

    feat: RegionCodeService에 finalCode 환경 변수 적용 및 중복 데이터 초기화 방지 로직 추가(#22)
    * application.yml에 region-code.init.final-code 설정 추가
    * finalCode를 @Value로 주입받아 중복 여부 판단에 활용
    * finalCode가 이미 존재하는 경우 초기화 로직 실행하지 않도록 조건 추가

--- src/main/java/jeonseguard/backend/region/domain/service/RegionCodeService.java
+++ src/main/java/jeonseguard/backend/region/domain/service/RegionCodeService.java
@@ -4,6 +4,8 @@ import jeonseguard.backend.region.domain.entity.RegionCode;
 import jeonseguard.backend.region.domain.parser.RegionCodeCsvParser;
 import jeonseguard.backend.region.domain.repository.RegionCodeRepository;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,8 +18,14 @@ public class RegionCodeService {
     private final RegionCodeRepository regionCodeRepository;
     private final RegionCodeCsvParser regionCodeCsvParser;
 
+    @Value("${region-code.init.final-code}")
+    private String finalCode;
+
     @Transactional
     public void saveNewRegionCodes(InputStream inputStream) {
+        if (regionCodeRepository.existsByCode(finalCode)) {
+            return;
+        }
         regionCodeRepository.saveAll(filterNewRegionCodes(inputStream));
     }
 
