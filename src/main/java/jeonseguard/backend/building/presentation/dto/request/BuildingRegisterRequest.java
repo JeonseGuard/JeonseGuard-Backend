fc62d11326b02131fcd3e603cd29dde09ab4670f
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-04-17 23:14:33 +0900

    feat: BuildingRegisterRequest 구현(#41)

---
+++ src/main/java/jeonseguard/backend/building/presentation/dto/request/BuildingRegisterRequest.java
@@ -0,0 +1,25 @@
+package jeonseguard.backend.building.presentation.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.*;
+
+public record BuildingRegisterRequest(
+        @Schema(description = "주소")
+        @NotBlank(message = "주소를 입력해 주세요.")
+        @Size(min = 10, max = 25, message = "주소는 10자 이상 25자 이하로 입력해 주세요.")
+        String address,
+
+        @Schema(description = "번")
+        @NotBlank(message = "번을 입력해 주세요.")
+        String bun,
+
+        @Schema(description = "지")
+        String ji,
+
+        @Schema(description = "동명칭")
+        String dongName,
+
+        @Schema(description = "호명칭")
+        String hoName
+) {
+}
