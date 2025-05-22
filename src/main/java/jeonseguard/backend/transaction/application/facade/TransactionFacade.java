f7a81041aa2cf64fd42e22f2b8eef409b7bd9048
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-05-22 20:42:18 +0900

    feat: (아파트, 오피스텔, 연립다세대) 매매 실거래가 조회 파사드 구현(#79)

--- src/main/java/jeonseguard/backend/transaction/application/facade/TransactionFacade.java
+++ src/main/java/jeonseguard/backend/transaction/application/facade/TransactionFacade.java
@@ -1,7 +1,7 @@
 package jeonseguard.backend.transaction.application.facade;
 
-import jeonseguard.backend.transaction.application.service.TransactionJeonseService;
-import jeonseguard.backend.transaction.presentation.dto.request.TransactionJeonseAddressRequest;
+import jeonseguard.backend.transaction.application.service.*;
+import jeonseguard.backend.transaction.presentation.dto.request.*;
 import jeonseguard.backend.transaction.presentation.dto.response.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,6 +12,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TransactionFacade {
     private final TransactionJeonseService jeonseService;
+    private final TransactionSaleService saleService;
 
     private static List<String> CONTRACT_YEAR_MONTHS = List.of(
             "202501", "202502", "202503", "202504"
@@ -32,6 +33,24 @@ public class TransactionFacade {
     public List<TransactionJeonseRowhouseResponse> getTransactionJeonseHistoryForRowhouse(TransactionJeonseAddressRequest request) {
         return jeonseService.getTransactionJeonseHistoryForRowhouse(request, CONTRACT_YEAR_MONTHS).stream()
                 .map(TransactionJeonseRowhouseResponse::fromEntity)
+                .toList();
+    }
+
+    public List<TransactionSaleApartmentResponse> getTransactionSaleHistoryForApartment(TransactionSaleAddressRequest request) {
+        return saleService.getTransactionSaleHistoryForApartment(request, CONTRACT_YEAR_MONTHS).stream()
+                .map(TransactionSaleApartmentResponse::fromEntity)
+                .toList();
+    }
+
+    public List<TransactionSaleOfficetelResponse> getTransactionSaleHistoryForOfficetel(TransactionSaleAddressRequest request) {
+        return saleService.getTransactionSaleHistoryForOfficetel(request, CONTRACT_YEAR_MONTHS).stream()
+                .map(TransactionSaleOfficetelResponse::fromEntity)
+                .toList();
+    }
+
+    public List<TransactionSaleRowhouseResponse> getTransactionSaleHistoryForRowhouse(TransactionSaleAddressRequest request) {
+        return saleService.getTransactionSaleHistoryForRowhouse(request, CONTRACT_YEAR_MONTHS).stream()
+                .map(TransactionSaleRowhouseResponse::fromEntity)
                 .toList();
     }
 }
